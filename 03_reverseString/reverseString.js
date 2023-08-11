233ffc9e9be53aa7cff7e5a78678ca359b8e010b
astylle <100005741+astylle@users.noreply.github.com>
2023-08-12 00:04:49 +0100

    Learning functions (1-7)
    Completed files tasks 1-7 of JS functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let phraseSegment = phrase.split('');
+    let reverseSegment = [];
+    for(let i = phraseSegment.length; i>=0;i--){
+        reverseSegment.push(phraseSegment[i]);
+    }    
+    let reversePhrase = reverseSegment.join("");
+    return reversePhrase;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    if(num1<num2){
+        let start=num1;
+        let end = num2;
+        return calculate(start, end);
+    }else{
+        let start =num2;
+        let end = num1;
+        return calculate(start, end);
+    }
+    };
+    function calculate(start, end){
+        let total = 0;
+        if(start>=0 && Number.isInteger(start) && Number.isInteger(end)){
+            for(let i=start; i<= end; i++){
+                total += i;
+            } return total;
+        }else
+        
+        return "ERROR";
+    }
 
 // Do not edit below this line
 module.exports = sumAll;
